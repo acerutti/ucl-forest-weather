07e2b6b468c05657cc67288054ef45cb83b3e284
Ale Cerutti <ale.eu.eli.ce@hotmail.it>
2024-03-11 18:11:36 +0000

    new folder for weather

--- template_specification_gcp_postgres.py
+++ template_specification_gcp_postgres.py
@@ -7,7 +7,7 @@ region = "us-central1"
 instance_name = "YOUR_INSTANCE_NAME"
 current_user = ['user@gmail.com']
 
-DB_USER = "DB_USER_HERE"
+DB_USER = "DB_USER_HERE" 
 DB_PASS = "DB_PASSWORD_HERE"
 DB_NAME = "DB_NAME_HERE"
 
